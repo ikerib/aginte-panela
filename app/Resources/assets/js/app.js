df817e5a05a21d0624c1d4a4173be02ed2bfd7fd
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2017-09-29 12:44:24 +0200

    updated vendors

--- app/Resources/assets/js/app.js
+++ app/Resources/assets/js/app.js
@@ -6,35 +6,51 @@ $(function () {
 
 });
 
-function initAjaxForm() {
-    $('body').on('submit', '.ajaxForm', function (e) {
-
-        e.preventDefault();
-
-
-        $.ajax({
-            type: $(this).attr('method'),
-            url: $(this).attr('action'),
-            data: $(this).serialize()
-        })
-        .done(function (data) {
-            if (typeof data.message !== 'undefined') {
-                alert(data.message);
-            }
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            if (typeof jqXHR.responseJSON !== 'undefined') {
-                if (jqXHR.responseJSON.hasOwnProperty('form')) {
-                    $('#form_body').html(jqXHR.responseJSON.form);
-                }
-
-                $('.form_error').html(jqXHR.responseJSON.message);
-
-            } else {
-                alert(errorThrown);
-            }
-
-        });
-    });
-}
+// function initAjaxForm() {
+//     $('body').on('submit', '.ajaxForm', function (e) {
+//
+//         e.preventDefault();
+//
+//
+//         $.ajax({
+//             type: $(this).attr('method'),
+//             url: $(this).attr('action'),
+//             data: $(this).serialize()
+//         })
+//         .done(function (data) {
+//             if (typeof data.message !== 'undefined') {
+//                 alert(data.message);
+//             }
+//
+// console.log("appenddddd");
+//
+//             $('#alertdiv').append(' ' +
+//                 '<div class="col-sm-3"></div>\n' +
+//                 '        <div class="col-sm-6">\n' +
+//                 '            <div id="alertSuccess" class="alert alert-success alert-dismissible">\n' +
+//                 '                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>\n' +
+//                 '                <h4><i class="icon fa fa-check"></i> Oharra!</h4>\n' +
+//                 '                Datuak ongi grabatuak izan dira.\n' +
+//                 '            </div>\n' +
+//                 '        </div>\n' +
+//                 '        <div class="col-sm-3">' +
+//                 '</div>').delay(1500).fadeOut();
+//
+//         })
+//         .fail(function (jqXHR, textStatus, errorThrown) {
+//             if (typeof jqXHR.responseJSON !== 'undefined') {
+//                 if (jqXHR.responseJSON.hasOwnProperty('form')) {
+//                     $('#form_body').html(jqXHR.responseJSON.form);
+//                 }
+//
+//                 $('.form_error').html(jqXHR.responseJSON.message);
+//
+//             } else {
+//
+//                 alert(errorThrown);
+//             }
+//
+//         });
+//     });
+// }
 
